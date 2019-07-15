521cb8e5a2fa88345ad512447ef97d722ad9e988
slwulf <atwulf@gmail.com>
2019-07-14 19:18:39 -0600

    add to default map

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ import Game from './src/Game.js'
 import RenderingEngine from './src/Renderer/Engines/DOMString'
 
 const big = `
-BsU__________________RsP
-________________________
-________________________
-___b_r_r________________
-________________________
+BsU__________________WsS
+________b________wwww___
+____________________w___
+_____RsP____________wwww
+_______________________w
 ___________________r____
 ________________________
 `
@@ -25,10 +25,13 @@ var game = new Game([
   '_': Game.Blocks.BLANK,
   'b': Game.Blocks.ICONS.Baba,
   'r': Game.Blocks.ICONS.Rock,
+  'w': Game.Blocks.ICONS.Wall,
   'B': Game.Blocks.NOUNS.Baba,
   'R': Game.Blocks.NOUNS.Rock,
+  'W': Game.Blocks.NOUNS.Wall,
   's': Game.Blocks.JOINERS.Is,
   'P': Game.Blocks.PROPERTIES.Push,
+  'S': Game.Blocks.PROPERTIES.Stop,
   'U': Game.Blocks.PROPERTIES.You
 },
 new RenderingEngine())
