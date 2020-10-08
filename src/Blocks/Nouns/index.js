2060d23b8afa4a4df3114f78e46e1a0ebcfe2c8e
Georg Wechslberger <wechslbe@ma.tum.de>
2020-10-08 03:37:07 +0200

    link each noun block to their corresponding icon block

--- src/Blocks/Nouns/index.js
+++ src/Blocks/Nouns/index.js
@@ -7,11 +7,41 @@ class NounBase extends BlockBase {
   }
 }
 
+class Baba extends NounBase {
 class Baba extends NounBase {}
 class Rock extends NounBase {}
 class Wall extends NounBase {}
 class Flag extends NounBase {}
 class Skull extends NounBase {}
 class Water extends NounBase {}
+  iconBlock() {
+    return Icons.Baba
+  }
+}
+class Rock extends NounBase {
+  iconBlock() {
+    return Icons.Rock
+  }
+}
+class Wall extends NounBase {
+  iconBlock() {
+    return Icons.Wall
+  }
+}
+class Flag extends NounBase {
+  iconBlock() {
+    return Icons.Flag
+  }
+}
+class Skull extends NounBase {
+  iconBlock() {
+    return Icons.Skull
+  }
+}
+class Water extends NounBase {
+  iconBlock() {
+    return Icons.Water
+  }
+}
 
 export default {Baba, Rock, Wall, Flag, Skull, Water}
